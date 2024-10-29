a7068722ccb5625bceb9d16f3739b1fc049b1ee2
tiffannyylee <tifflee@byu.edu>
2024-10-28 18:17:53 -0600

    add code for implementation tests

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -69,4 +69,13 @@ public class DatabaseManager {
             throw new DataAccessException(e.getMessage());
         }
     }
+    public void example() throws Exception {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("SELECT 1+1")) {
+                var rs = preparedStatement.executeQuery();
+                rs.next();
+                System.out.println(rs.getInt(1));
+            }
+        }
+    }
 }
