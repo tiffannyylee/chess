fbf981e93abc8e530d3613d605c26d3ea3336962
tiffannyylee <tifflee@byu.edu>
2024-11-12 13:59:04 -0700

    DELETE unnecessary text

--- client/src/main/java/PreLoginClient.java
+++ client/src/main/java/PreLoginClient.java
@@ -4,12 +4,6 @@ import model.UserData;
 import java.util.Arrays;
 
 public class PreLoginClient {
-    //    Help	Displays text informing the user what actions they can take.
-//Quit	Exits the program.
-//Login	Prompts the user to input login information. Calls the server login API to login the user.
-// When successfully logged in, the client should transition to the Postlogin UI.
-//Register	Prompts the user to input registration information. Calls the server register API to register and login the user.
-// If successfully registered, the client should be logged in and transition to the Postlogin UI.
     private final ServerFacade server;
     private final String serverUrl;
 
