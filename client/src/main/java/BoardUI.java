1b3b80e82e58950355d7652280c85534442ca41b
tiffannyylee <tifflee@byu.edu>
2024-11-13 19:03:12 -0700

    swap headers and numbers

--- client/src/main/java/BoardUI.java
+++ client/src/main/java/BoardUI.java
@@ -32,35 +32,45 @@ public class BoardUI {
             {BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_QUEEN, BLACK_KING, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK}
     };
 
-    public static void drawColumnHeaders(PrintStream out) {
-        out.print("    ");
-        for (char col = 'a'; col < 'i'; col++) {
-            out.printf("  %c  ", col);
+    public static void drawColumnHeaders(PrintStream out, String[][] board) {
+        if (board == INITIAL_BOARD_WHITE) {
+            out.print("    ");
+            for (char col = 'a'; col < 'i'; col++) {
+                out.printf("  %c  ", col);
+            }
+            out.println();
+        } else {
+            out.print("    ");
+            for (char col = 'h'; col >= 'a'; col--) {
+                out.printf("  %c  ", col);
+            }
+            out.println();
         }
-        out.println();
+
     }
 
 
     public static void drawChessBoard(PrintStream out, String[][] board) {
-        drawColumnHeaders(out);
-
-        // Draw each row of the board
+        drawColumnHeaders(out, board);
         for (int row = 0; row < BOARD_SIZE; row++) {
+            int displayRow = board == INITIAL_BOARD_WHITE ? BOARD_SIZE - row : row + 1;
+
             // Repeat each square row to create a square
             for (int h = 0; h < SQUARE_HEIGHT; h++) {
                 // Add row numbers on the left side only in the middle of the square height
                 if (h == SQUARE_HEIGHT / 2) {
-                    out.printf(" %d  ", BOARD_SIZE - row);
+                    out.printf(" %d  ", displayRow);
                 } else {
                     out.print("    ");
                 }
 
                 // Draw each column in the row
                 for (int col = 0; col < BOARD_SIZE; col++) {
+                    int displayCol = board == INITIAL_BOARD_WHITE ? col : BOARD_SIZE - col - 1;
                     String color = (row + col) % 2 == 0 ? LIGHT_COLOR : DARK_COLOR;
 
                     if (h == SQUARE_HEIGHT / 2) {  // Middle row to display piece
-                        out.print(color + board[row][col] + EMPTY_SQUARE + RESET_COLOR);
+                        out.print(color + board[row][displayCol] + EMPTY_SQUARE + RESET_COLOR);
                     } else {  // Empty padding rows for height
                         out.print(color + "     " + RESET_COLOR);
                     }
@@ -68,13 +78,13 @@ public class BoardUI {
 
                 // Add row numbers on the right side
                 if (h == SQUARE_HEIGHT / 2) {
-                    out.printf("  %d", BOARD_SIZE - row);
+                    out.printf("  %d", displayRow);
                 }
                 out.println();
             }
         }
 
-        drawColumnHeaders(out);
+        drawColumnHeaders(out, board);
     }
 
     public static void drawChessBoardWhite(PrintStream out) {
