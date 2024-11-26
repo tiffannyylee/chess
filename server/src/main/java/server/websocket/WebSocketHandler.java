fefca1543ebbda367d9c3f272f6558eded698c46
tiffannyylee <tifflee@byu.edu>
2024-11-26 11:25:46 -0700

    comment out game plan for websocket handler

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,9 +1,53 @@
 package server.websocket;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.UserGameCommand;
 
+import java.io.IOException;
+
+@WebSocket
 public class WebSocketHandler {
+    ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        switch (command.getCommandType()) {
+            case CONNECT -> connect();
+            case MAKE_MOVE -> makeMove();
+            case LEAVE -> leave();
+            case RESIGN -> resign();
+        }
+
+    }
+
+    private void connect() {
+        //connect to the connections manager
+        //send load game to root
+        //broadcast notification that root joined
+    }
+
+    private void makeMove() {
+        //verify move is valid
+        //update game to show move and update game in database
+        //send a load game to all clients
+        //send notification to others about what move was made
+        //send notification ab check checkmate or stalemate to all
+    }
+
+    private void leave() {
+        //remove client from connections
+        //update game in database
+        //send notification to all players and observers that root left
+    }
+
+    private void resign() {
+        //server marks the game as over
+        //game is updated in the database
+        //sends notification to all players that root resigned. both players and observers
+    }
 
 }
