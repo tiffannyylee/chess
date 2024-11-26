16bb18456a3bfe48fe63582a52630617cc27b39d
tiffannyylee <tifflee@byu.edu>
2024-11-26 11:42:19 -0700

    make move command

---
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -0,0 +1,19 @@
+package websocket.commands;
+
+import chess.ChessMove;
+
+public class MakeMoveCommand extends UserGameCommand {
+    private final ChessMove move;
+
+    public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID, ChessMove move) {
+        super(commandType, authToken, gameID);
+        if (commandType != CommandType.MAKE_MOVE) {
+            throw new IllegalArgumentException("MakeMoveCommand must have commandType MAKE_MOVE");
+        }
+        this.move = move;
+    }
+
+    public ChessMove getMove() {
+        return move;
+    }
+}
--- shared/src/main/java/websocket/commands/UserGameCommand.java
+++ shared/src/main/java/websocket/commands/UserGameCommand.java
@@ -1,10 +1,12 @@
 package websocket.commands;
 
+import chess.ChessMove;
+
 import java.util.Objects;
 
 /**
  * Represents a command a user can send the server over a websocket
- *
+ * <p>
  * Note: You can add to this class, but you should not alter the existing
  * methods.
  */
