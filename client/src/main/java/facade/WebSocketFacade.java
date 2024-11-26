480038189a81a1b075f647a211e5d6d780db5f5e
tiffannyylee <tifflee@byu.edu>
2024-11-26 08:48:03 -0700

    start with writing connection

--- client/src/main/java/facade/WebSocketFacade.java
+++ client/src/main/java/facade/WebSocketFacade.java
@@ -1,4 +1,7 @@
 package facade;
 
 public class WebSocketFacade {
+    //start connection when websocket object is created
+    // have a place for breaking down the message sent from server
+    //write user command messages that send to the server
 }
--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,4 +1,9 @@
 package server.websocket;
 
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
 public class WebSocketHandler {
+
 }
