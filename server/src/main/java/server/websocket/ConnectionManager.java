cbf74b1787fa089a7bb5e6a2c6833d51942f6659
tiffannyylee <tifflee@byu.edu>
2024-11-26 09:17:55 -0700

    connection manager code

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,4 +1,37 @@
 package server.websocket;
 
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ConnectionManager {
+    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+    public void add(String userName, Session session) {
+        var connection = new Connection(userName, session);
+        connections.put(userName, connection);
+    }
+
+    public void delete(String userName) {
+        connections.remove(userName);
+    }
+
+    public void broadcast(String excludeUserName, ServerMessage message) throws IOException {
+        ArrayList<Connection> badConnections = new ArrayList<>();
+        for (Connection c : connections.values()) {
+            if (c.session.isOpen()) {
+                if (!c.userName.equals(excludeUserName)) {
+                    c.send(message.toString());
+                }
+            } else {
+                badConnections.add(c);
+            }
+        }
+        for (var c : badConnections) {
+            connections.remove(c.userName);
+        }
+    }
 }
